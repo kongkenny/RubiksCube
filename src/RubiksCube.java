04a69d4c3ed4c28d518c300d6344ae436b41c537
kongkenny <kong.kenny@gmail.com>
2017-05-19 20:47:48 -0700

    Changed Rubik's Cube data structure to use hashmap. Added turn methods declarations.

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -1,24 +1,94 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class RubiksCube
 {
-	private Side[] sides;
+	private HashMap<String, Side> sides;
+	private int[] strip;
 	
 	public RubiksCube()
 	{
-		sides = new Side[6];
-		sides[0] = new Side(1);
-		sides[1] = new Side(2);
-		sides[2] = new Side(3);
-		sides[3] = new Side(4);
-		sides[4] = new Side(5);
-		sides[5] = new Side(6);
+		sides = new HashMap<String, Side>();
+		sides.put("U", new Side(0));
+		sides.put("F", new Side(1));
+		sides.put("R", new Side(2));
+		sides.put("B", new Side(3));
+		sides.put("L", new Side(4));
+		sides.put("D", new Side(5));
+		
+		strip = new int[3];
+	}
+	
+	public void U()
+	{
+		int temp = -1;
+	}
+	
+	public void Ui()
+	{
+		int temp = -1;
+	}
+	
+	public void D()
+	{
+		int temp = -1;
+	}
+	
+	public void Di()
+	{
+		int temp = -1;
+	}
+	
+	public void F()
+	{
+		int temp = -1;
+	}
+	
+	public void Fi()
+	{
+		int temp = -1;
+	}
+	
+	public void B()
+	{
+		int temp = -1;
+	}
+	
+	public void Bi()
+	{
+		int temp = -1;
+	}
+	
+	public void L()
+	{
+		int temp = -1;
+	}
+	
+	public void Li()
+	{
+		int temp = -1;
+	}
+	
+	public void R()
+	{
+		int temp = -1;
+	}
+	
+	public void Ri()
+	{
+		int temp = -1;
 	}
 	
 	public String toString()
 	{
 		String result = "";
-		for (int i = 0; i < sides.length; i++)
-			result += sides[i].toString() + "\n";
+		Set<String> set = sides.keySet();
+		for (Iterator<String> i = set.iterator(); i.hasNext();)
+		{
+			String side = i.next();
+			result += side + "\n" + sides.get(side).toString() + "\n";
+		}
 		return result;
 	}
 }
