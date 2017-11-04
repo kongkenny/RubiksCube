132b92b4ed8d0ece54fcd356d01071595f3d0586
Kenny Kong <kong.kenny@gmail.com>
2017-11-04 16:21:24 +0100

    Resolved pull conflict.

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -102,105 +101,16 @@ public class RubiksCube
 	public void DCC()
 	{
 		
-=======
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
-
-public class RubiksCube
-{
-	private HashMap<String, Side> sides;
-	private int[] strip;
-	
-	public RubiksCube()
-	{
-		sides = new HashMap<String, Side>();
-		sides.put("U", new Side(0));
-		sides.put("F", new Side(1));
-		sides.put("R", new Side(2));
-		sides.put("B", new Side(3));
-		sides.put("L", new Side(4));
-		sides.put("D", new Side(5));
-		
-		strip = new int[3];
-	}
-	
-	public void U()
-	{
-		int temp = -1;
-	}
-	
-	public void Ui()
-	{
-		int temp = -1;
-	}
-	
-	public void D()
-	{
-		int temp = -1;
-	}
-	
-	public void Di()
-	{
-		int temp = -1;
-	}
-	
-	public void F()
-	{
-		int temp = -1;
-	}
-	
-	public void Fi()
-	{
-		int temp = -1;
-	}
-	
-	public void B()
-	{
-		int temp = -1;
-	}
-	
-	public void Bi()
-	{
-		int temp = -1;
-	}
-	
-	public void L()
-	{
-		int temp = -1;
-	}
-	
-	public void Li()
-	{
-		int temp = -1;
-	}
-	
-	public void R()
-	{
-		int temp = -1;
-	}
-	
-	public void Ri()
-	{
-		int temp = -1;
->>>>>>> 04a69d4c3ed4c28d518c300d6344ae436b41c537
 	}
 	
 	public String toString()
 	{
 		String result = "";
-<<<<<<< HEAD
+
 		for (String key : sides.keySet())
 		{
 			result += key + "\n";
 			result += sides.get(key) + "\n";
-=======
-		Set<String> set = sides.keySet();
-		for (Iterator<String> i = set.iterator(); i.hasNext();)
-		{
-			String side = i.next();
-			result += side + "\n" + sides.get(side).toString() + "\n";
->>>>>>> 04a69d4c3ed4c28d518c300d6344ae436b41c537
 		}
 		return result;
 	}
